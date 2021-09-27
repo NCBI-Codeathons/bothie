92480274b40d3ce73e7ad050e59ee4ad8ca2b68a
N Tessa Pierce <ntpierce@gmail.com>
2021-09-27 10:12:01 -0700

    upd for new structure

--- runme.py
+++ runme.py
@@ -1,4 +1,5 @@
 #! /usr/bin/env python3
+import os
 import subprocess
 import datetime
 import shutil
@@ -8,9 +9,10 @@ import argparse
 def main():
     p = argparse.ArgumentParser()
     p.add_argument('config_file')
+    p.add_argument('tool', default='sourmash')
     p.add_argument('query')
     args = p.parse_args()
-    
+
     print('removing outputs/')
     try:
         shutil.rmtree('./outputs/')
@@ -18,21 +20,23 @@ def main():
         pass
 
     print('installing software:')
-    subprocess.check_call(f'snakemake -j 1 --use-conda install_software --configfile {args.config_file} -C query_file={args.query}',
-                          shell=True)
+    tool_dir = args.tool
+    config_file = os.path.abspath(args.config_file)
+    subprocess.check_call(f'snakemake -j 1 --use-conda install_software --configfile {config_file} -C query_file={args.query}',
+                          shell=True, cwd=tool_dir)
 
     print(datetime.datetime.now(), 'START SAMPLE PREP')
-    subprocess.check_call(f'snakemake -j 1 --use-conda prepare_sample  --configfile {args.config_file} -C query_file={args.query}',
-                          shell=True)
+    subprocess.check_call(f'snakemake -j 1 --use-conda prepare_sample  --configfile {config_file} -C query_file={args.query}',
+                          shell=True, cwd=tool_dir)
     print(datetime.datetime.now(), 'END SAMPLE PREP')
 
     print(datetime.datetime.now(), 'START QUERY PREP')
-    subprocess.check_call(f'snakemake -j 1 --use-conda prepare_query --configfile {args.config_file} -C query_file={args.query}',
-                          shell=True)
+    subprocess.check_call(f'snakemake -j 1 --use-conda prepare_query --configfile {config_file} -C query_file={args.query}',
+                          shell=True, cwd=tool_dir)
     print(datetime.datetime.now(), 'END QUERY PREP')
 
     print(datetime.datetime.now(), 'START DO QUERY')
-    subprocess.check_call(f'snakemake -j 1 --use-conda do_query --configfile {args.config_file} -C query_file={args.query}', shell=True)
+    subprocess.check_call(f'snakemake -j 1 --use-conda do_query --configfile {config_file} -C query_file={args.query}', shell=True, cwd=tool_dir)
     print(datetime.datetime.now(), 'END DO QUERY')
 
 if __name__ == '__main__':
