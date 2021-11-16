cd426feb45ba85d64482fce4a9870285c34e7a5e
Rene <rmillerxavie2017@fau.edu>
2021-11-16 14:15:56 -0500

    Test DNA search on Mock HAB dataset

--- run-many-tools.sh
+++ run-many-tools.sh
@@ -3,8 +3,12 @@
 #This tool uses the data/data.csv file to locate the queries and samples.
 #Then it runs each tool in turn: sourmash, sourmash-protein, cmash, mash-screen, metagraph
 
-./run-many-csv sourmash/conf-basic.yml sourmash data/mock_datasets.csv -o sourmash_out.csv
-./run-many-csv sourmash-protein/conf-basic.yml sourmash-protein data/mock_datasets.csv -o sourmash_protein_out.csv
-./run-many-csv cmash/conf-basic.yml cmash data/mock_datasets.csv -o cmash-out.csv
-./run-many-csv mash/conf-basic.yml mash data/mock_datasets.csv -o mash_out.csv
-./run-many-csv metagraph/conf-basic.yml metagraph data/mock_datasets.csv -o metagraph-out.csv
+set -x
+set -e
+
+for tool in sourmash mash cmash metagraph sourmash-protein
+do
+    ./run-many-csv ${tool}/conf-HAB.yml $tool data/mock_datasets.csv -o HAB-${tool}.csv
+done
+
+echo success!
